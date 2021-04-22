1a2cca4830973a97d9ddc3a40ef26f9e21b5bb76
CharlesB83 <charlybiancheri99@gmail.com>
2021-04-22 11:54:29 +0200

    adds cb.java

---
+++ src/main/java/fr/univtln/hgitinit/cb.java
@@ -0,0 +1,14 @@
+package fr.univtln.hgitinit;
+
+/**
+ * Hello world!
+ *
+ */
+public class CB
+{
+    public static void main( String[] args )
+    {
+        for (int i = 0; i < 5; i++)
+        	System.out.println( "Coucouuu" );
+    }
+}
