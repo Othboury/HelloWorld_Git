e35b3d3a7e3592c43aa3fd6abd0c023c54ab9134
Tiffany83140 <tiffanyc83@live.fr>
2021-04-22 11:55:34 +0200

    ajout de la classe hello

---
+++ src/main/java/fr/univtln/hgitinit/Hello.java
@@ -0,0 +1,13 @@
+package fr.univtln.hgitinit;
+
+public class Hello {
+
+    public static void hello(){
+        System.out.println("Hello World !");
+    }
+
+    public static void main(String[] args) {
+        hello();
+    }
+
+}
